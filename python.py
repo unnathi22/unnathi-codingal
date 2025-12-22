c0a746a68f7428624e09386dd998cbcd52631afd
unnathi22 <shilpaghanavalli@gmail.com>
2025-12-22 19:56:11 +0530

    firstcode.py

---
+++ python.py
@@ -0,0 +1,10 @@
+#hey i am printing my name
+print("my name is unnathi")
+#i am printing pincode
+print("570068")
+#assigning value to X
+x=5
+#adding 3 to x
+print(3+x)
+#making an equation
+print("3+x")
