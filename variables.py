54703109e146b5965283c9a08cfab5517d7ed53e
unnathi22 <shilpaghanavalli@gmail.com>
2025-12-27 19:58:21 +0530

    creating variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+#creating different variables 
+raman="3 may 2012"
+riya="5 jan 2013"
+hinara="5 may 2012"
+aman="8 aug 2012"
+anjali="19 sep 2009"
+print("these are my friend's birthday dates :\n3raman,riya,hinara,aman and anjali")
+#printing the variables
+print(raman)
+print(riya)
+print(hinara)
+print(aman)
+print(anjali)
