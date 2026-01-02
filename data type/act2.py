296cfcff02eeda02729758525f8e67621957c1fa
unnathi22 <shilpaghanavalli@gmail.com>
2026-01-02 20:57:19 +0530

    data type 2

---
+++ data type/act2.py
@@ -0,0 +1,12 @@
+name="unnathi"
+grade=8
+height=5.2
+isstudent=True
+print(type(name))
+print(type(grade))
+print(type(height))
+print(type(isstudent))
+print("after typecasting")
+print(float(grade))
+print(int(height))
+print(str(grade))
