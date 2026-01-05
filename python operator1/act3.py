59b114178f6b35e59db2c9c6da4adeb52ba780fc
unnathi22 <shilpaghanavalli@gmail.com>
2026-01-05 20:22:19 +0530

    calculating percentage

---
+++ python operator1/act3.py
@@ -0,0 +1,7 @@
+#obtaining the numbers from the user
+math=int(input("math:"))
+english=int(input("english:"))
+science=int(input("science:"))
+sum=math+english+science
+per=(sum/300)*100
+print(per)
