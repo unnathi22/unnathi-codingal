af6ddec96231457d40244b8cb1e392c56268b267
unnathi22 <shilpaghanavalli@gmail.com>
2026-01-08 20:44:16 +0530

    greater and smaller

---
+++ python operator1/if else/act3.py
@@ -0,0 +1,8 @@
+i=int(input("enter the number:"))
+if i<15:
+    print("the number entered is less than 15!")
+    print(i,"is in if block")
+else:
+    print("the number entered is greater than 15")
+    print(i,"is in else block")
+print("i am going to check the number whether it is greater or smaller")
