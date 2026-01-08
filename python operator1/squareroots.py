a1b204e71b74c8df9f9f94d6816805dcaeb7179d
unnathi22 <shilpaghanavalli@gmail.com>
2026-01-08 17:30:25 +0530

    squareroots

--- python operator1/squareroots.py
+++ python operator1/squareroots.py
@@ -1,6 +1,6 @@
 #asking the input from the user
 number=int(input("please enter a number to find the square root:"))
 #to calculate the sqaure root of the number obtained
-sqroot=(number**2)
+sqroot=(number**0.5)
 #getting the answer for the input digit
 print(sqroot)
