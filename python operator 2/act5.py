77a242d903d71e9cab65768251635a5eaaea1f85
unnathi22 <shilpaghanavalli@gmail.com>
2026-01-09 21:30:31 +0530

    alphabet finding

---
+++ python operator 2/act5.py
@@ -0,0 +1,7 @@
+character=input("enter the charcater!! :")
+if character=="abc":
+    print("got it ! its an alphabet !")
+elif character=="123":
+    print("oh ! it seems like you have added a digit !!")
+else:
+    print("it isnt an alphabet!!")
