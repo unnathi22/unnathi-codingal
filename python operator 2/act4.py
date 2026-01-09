24aaa2755acbf61f2a2fd108f321a27a6a6efd66
unnathi22 <shilpaghanavalli@gmail.com>
2026-01-09 21:22:22 +0530

    summer times

---
+++ python operator 2/act4.py
@@ -0,0 +1,5 @@
+whether=input("enter the whether of your place to find out the perfect clothes to wear according to your surrounding !!:")
+if whether=="cold":
+    print("oh ! then wear woolen and warm clothes to make you feel warmer !")
+else:
+    print("its hot ! then wear something light and comfortable for the sunny day !!")
