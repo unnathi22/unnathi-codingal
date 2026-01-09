b5679f14246317939a51bd2f5275c8ddf112f012
unnathi22 <shilpaghanavalli@gmail.com>
2026-01-09 20:23:48 +0530

    python 2

---
+++ python operator 2/act1.py
@@ -0,0 +1,14 @@
+a=5
+b=0
+c=7
+if a and b and c:
+    print("either one of them is 0")
+else:
+    print("none of them are 0")
+a=5
+b=5
+c=0
+if a or b or c:
+    print("one of them is not zero")
+else:
+    print("everything is zero")
