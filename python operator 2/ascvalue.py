f507be9bdcb284d7716519c117d5b7e98fdd0e0b
unnathi22 <shilpaghanavalli@gmail.com>
2026-01-27 20:01:29 +0530

    ASCII values

---
+++ python operator 2/ascvalue.py
@@ -0,0 +1,8 @@
+#assigning the values in print
+print(ord('a')) #output will be 97
+print(ord('A')) #output will be 65
+print(ord('0')) #output will be 48
+print(ord('@')) #output will be 64
+#converting the ASCII value to the character
+print(chr(90))
+print(chr(67))
