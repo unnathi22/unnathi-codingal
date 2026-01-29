66bbfa9f2e61580de9d68a5834e81a501b42218b
unnathi22 <shilpaghanavalli@gmail.com>
2026-01-29 20:22:25 +0530

    division

---
+++ python operator 2/divison.py
@@ -0,0 +1,6 @@
+num=int(input("enter the numerator"))
+den=int(input("enter the denominator"))
+if num%den==0:
+    print("it is divisible by the number")
+else:
+    print("it is not divisible by the number")
