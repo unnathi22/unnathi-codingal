8c2e2c1066273e00dd59717a0828586aba26d15f
unnathi22 <shilpaghanavalli@gmail.com>
2026-01-30 20:29:26 +0530

    ifelse

---
+++ cost.py
@@ -0,0 +1,9 @@
+unit=int(input("enter the amount :"))
+if unit<=50:
+    print("the entered digit is less than 50")
+elif unit<=90:
+    print("the entered number is less than 90")
+elif unit<=150:
+    print("the entered number is less than 150 !")
+else:
+    print("the number is greater")
