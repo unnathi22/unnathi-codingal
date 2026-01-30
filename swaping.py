72f645e24dc121b38cbacb9b41e4c72c7bca78d7
unnathi22 <shilpaghanavalli@gmail.com>
2026-01-30 19:42:56 +0530

    swaping

---
+++ swaping.py
@@ -0,0 +1,10 @@
+#creating the variables
+x=10
+y=20
+z=30
+print(f"before swaping the numbers :X={x}, Y={y},Z={z}")
+#swaping the numbers using a temporary variable
+temp=x
+x=z
+z= temp
+print(f"after swaping the numbers :X={x},Y={y},Z={z}")
