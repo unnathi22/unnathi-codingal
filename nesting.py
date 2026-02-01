862c56ab25a109e689aeb9e79055a508d397c963
unnathi22 <shilpaghanavalli@gmail.com>
2026-02-01 22:08:32 +0530

    nestingg

---
+++ nesting.py
@@ -0,0 +1,8 @@
+#getting an input from the user
+age = int(input("Enter student age: "))
+if 10 <= age <= 20:
+     print("Student allowed in class.")
+else:
+     print("Student not allowed in class. Age limit is 10 to 20 years !.")
+
+    
