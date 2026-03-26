eb720496d3b63f2ec46e34155aad432d9ef9c7e5
unnathi22 <shilpaghanavalli@gmail.com>
2026-03-26 23:03:35 +0530

    dueamt

---
+++ python operator 2/actt1.py/dueamt.py
@@ -0,0 +1,4 @@
+billamt=float(input("enter the total amount of the item:"))
+paid=float(input("enter the amount paid"))
+dueamount=billamt-paid
+print("leftover amount to be paid by you is :",dueamount)
