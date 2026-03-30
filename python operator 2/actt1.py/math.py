62516f9858f754dcbba97113f9ce9ce6b575ab04
unnathi22 <shilpaghanavalli@gmail.com>
2026-03-30 20:28:33 +0530

    mathhs

---
+++ python operator 2/actt1.py/math.py
@@ -0,0 +1,12 @@
+import math
+print(math.floor(8.3))
+print(math.ceil(8.3))
+x=2
+y=-3
+print(math.copysign(x,y))
+print(math.fabs(8.1))
+print(math.gcd(4,6))
+print(math.sqrt(2))
+print(math.pow(2,3))
+
+
