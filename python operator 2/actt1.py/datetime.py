0ea2def5e09e777028be31767d3bf98af6a50415
unnathi22 <shilpaghanavalli@gmail.com>
2026-03-31 20:28:53 +0530

    datetimez

--- python operator 2/actt1.py/datetime.py
+++ python operator 2/actt1.py/datetime.py
@@ -1,6 +1,11 @@
-from datetime import *
-today=date.today()
-now=datetime.now()
-print("today's date is :",today)
-print("time is :",now)
-print("date components:",today.year,today.month,today.day)
+import random
+import time
+def getrandomdate(startdate,enddate):
+    print("generating a random date.....")
+    randomgenerator=random.random()
+    dateformat="%m/%d/%Y"
+    startdate=time.mktime(time.strptime(startdate,dateformat))
+    enddate=time.mktime(time.strptime(enddate,dateformat))
+    randomtime = startdate+ randomgenerator * (enddate - startdate)
+    return randomtime
+print(getrandomdate("1/1/2016","12/12/2018"))
