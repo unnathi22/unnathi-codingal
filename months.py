3fc24d32fecd3a4a6c2ed3798ae0bb7dfe3ab2a3
unnathi22 <shilpaghanavalli@gmail.com>
2026-04-01 20:02:08 +0530

    months of year

---
+++ months.py
@@ -0,0 +1,10 @@
+import calendar
+print("names of all the months in an year:")
+for month in range(1,13):
+    print(calendar.month_name[month])
+
+   
+
+
+
+# 7) Ensure all 12 months are displayed as the final output.
