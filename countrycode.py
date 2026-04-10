808f5f23ed97c45628928d02500661a8acc8cf8e
unnathi22 <shilpaghanavalli@gmail.com>
2026-04-10 20:51:42 +0530

    countryy

---
+++ countrycode.py
@@ -0,0 +1,6 @@
+country_code={'India':'0091',
+              'Australia':'0021',
+              'Malasia':'0056'}
+print("country code for Australia:",country_code.get("Australia","not found"))
+print("country code for India:",country_code.get("India","not found")
+)
