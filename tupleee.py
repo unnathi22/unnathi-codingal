72ee49772ccbed8490dce500dd78230b922075f5
unnathi22 <shilpaghanavalli@gmail.com>
2026-04-14 21:23:28 +0530

    tupleee

---
+++ tupleee.py
@@ -0,0 +1,9 @@
+def tuple_multiply(tup):
+    result=1 
+    for num in tup:
+        result*=num
+    return result
+    tup1=(1,2,3,4,-5,-6,-7)
+    tup2=(1,2,3,4,5-2-1-3)
+    print(tuple_product(tup1))
+    print(tuple_product(tup1))
