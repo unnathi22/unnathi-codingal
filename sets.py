d7fca1e5e9d805583f6fc3d06455adaaf797c11b
unnathi22 <shilpaghanavalli@gmail.com>
2026-04-15 20:15:46 +0530

    setssss

---
+++ sets.py
@@ -0,0 +1,12 @@
+set1={'pink','blue','green','ocean blue','pink'}
+set2={'pink','red','cherry red','gray','ocean blue'}
+print(set1)
+print(set2)
+print("the symmetric difference of set 1 and set2 are:")
+print(set1.symmetric_difference(set2))
+set3={1,2,3,4,5,6,6,6,7,6,7,6,7,6,7}
+set4={111,10,9,8,7,67,6,7,6,7,6,7,6,7}
+print(set3)
+print(set4)
+print("the symmetric difference of set3 and set4 are:")
+print(set3.symmetric_difference(set4))
