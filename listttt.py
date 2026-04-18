9229d9a1d7a88bb87613f87e626a5c578f9617ca
unnathi22 <shilpaghanavalli@gmail.com>
2026-04-18 21:20:32 +0530

    listtttt

---
+++ listttt.py
@@ -0,0 +1,8 @@
+list1=[1,2,3,4,5,2345,667788,67,67]
+print(list1)
+odd=print("the odd numbers are:",[x for x in list1 if x%2!=0])
+list2=[13131,12121,14141,4455669,5678,1,3,6,9,0]
+print(list2)
+odd2=print("the odd numbers are:",[x for x in list2 if x%2!=0])
+
+
