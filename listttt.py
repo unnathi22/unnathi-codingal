3b1e64ab03ca2e83b84d452d97b5310e9a72e9c5
unnathi22 <shilpaghanavalli@gmail.com>
2026-04-18 22:33:49 +0530

    listyyyy

--- listttt.py
+++ listttt.py
@@ -1,8 +1,10 @@
 list1=[1,2,3,4,5,2345,667788,67,67]
 print(list1)
-odd=print("the odd numbers are:",[x for x in list1 if x%2!=0])
-list2=[13131,12121,14141,4455669,5678,1,3,6,9,0]
-print(list2)
-odd2=print("the odd numbers are:",[x for x in list2 if x%2!=0])
+odd=[x for x in list1 if x%2!=0]
+print("the odd numbers are:",odd)
+list2=['passionfruit','dragonfruit','strawberry','pineapple']
+uppercase=list(map(str.upper,list2))
+print(uppercase)
+
 
 
