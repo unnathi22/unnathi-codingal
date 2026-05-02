c983650e679cadd6143c69ab8722987879e9e0e1
unnathi22 <shilpaghanavalli@gmail.com>
2026-05-02 13:29:40 +0530

    vehicleeee

--- vehicle.py
+++ vehicle.py
@@ -1,13 +1,15 @@
 class vehicle:
-    def __init__(self,seating_capacity,name):
-        self.seating_capacity=seating_capacity
+    def __init__(self,name,seating_capacity):
         self.name=name
-        totalfare=seating_capacity*100
-        print("total fare of the ride is :",totalfare)
+        self.seating_capacity=seating_capacity
+    def fare(self):
+        return self.seating_capacity*100
+        
 class bus(vehicle):
-    maintenence=totalfare*0.1
-obj=vehicle()
-obj.fare(100,50)
-obj.maintenence()
-
-    
+    def fare(self):
+        normal_fare=super().fare()
+        maintenence_charge=normal_fare*0.10
+        final_fare=maintenence_charge+normal_fare
+        return final_fare
+ob1=bus("school bus",50)
+print(f"final fare for :",ob1.name,"is",ob1.fare())
